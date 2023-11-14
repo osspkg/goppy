c0af357d8ee6ff4073986547dcf08d0b2247a689
Mikhail Knyazhev <markus621@gmail.com>
2023-11-14 23:56:10 +0300

    update lic info

--- xtest/errors.go
+++ xtest/errors.go
@@ -1,3 +1,8 @@
+/*
+ *  Copyright (c) 2022-2023 Mikhail Knyazhev <markus621@yandex.ru>. All rights reserved.
+ *  Use of this source code is governed by a BSD 3-Clause license that can be found in the LICENSE file.
+ */
+
 package xtest
 
 func NoError(t IUnitTest, err error, args ...interface{}) {
